8216a8ba3873696b81ef04d1cf7c32dec051bfff
zaharmihed <zahar.mihed@gmail.com>
2026-05-01 00:14:32 +0300

    First task

--- src/by/homework/lessons/task1/Main.java
+++ src/by/homework/lessons/task1/Main.java
@@ -4,4 +4,3 @@ public class lessen1 {
 
     }
 }
-
--- src/lessen1.java
+++ src/lessen1.java
@@ -2,6 +2,5 @@ public class lessen1 {
     public  static void main(String[] args) {
         System.out.printf("Михед, 22.04.2026, 19:46");
 
-     }
+    }
 }
-
