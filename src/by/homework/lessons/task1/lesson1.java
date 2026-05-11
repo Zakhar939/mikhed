9c6f0f40094c6fb9b6057ec57d1b9d83ba058ba0
zaharmihed <zahar.mihed@gmail.com>
2026-05-11 23:46:51 +0300

    First task

--- src/by/homework/lessons/task1/lesson1.java
+++ src/by/homework/lessons/task1/lesson1.java
@@ -1,6 +1,6 @@
 package by.homework.lessons.task1;
 
-public class lessen1 {
+public class lesson1 {
     public  static void main(String[] args) {
         System.out.println("Михед, 22.04.2026, 19:46");
 
