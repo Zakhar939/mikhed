545e2aea32fc0bdaed2de7f7b1ff107c67dbbd71
Zakhar Mikhed <zahar.mihed@gmail.com>
2026-06-22 22:40:52 +0300

    Delete src directory

--- src/by/homework/lessens/task1/lessen1.java
+++
@@ -1,8 +0,0 @@
-package by.homework.lessens.task1;
-
-public class lessen1 {
-    public  static void main(String[] args) {
-        System.out.println("Михед, 22.04.2026, 19:46");
-
-    }
-}
--- src/by/homework/lessens/task11/RobotEngineer.java
+++
@@ -1,6 +0,0 @@
-package by.homework.lessens.task11;
-
-public interface RobotEngineer extends Robot {
-    void createItem();
-
-}
